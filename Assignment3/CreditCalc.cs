0c34e56bd2a928dcb74f2b234fa64dc5e6609798
aparnakr5 <57255095+aparnakr5@users.noreply.github.com>
2019-11-08 15:30:35 +0530

    Add files via upload

---
+++ Assignment3/CreditCalc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class CreditCalc
+    {
+        public int Calc(double x, double y)
+        {
+            double due = x;
+            double mi = y;
+            double ir = 1.5d;
+            int dur = 0;
+            double payment = 0;
+            Console.WriteLine("********************");
+            Console.WriteLine("Current Due: " + due);
+            Console.WriteLine("***********************************************************************");
+            for (int i=1;due>=0;i++)
+            {
+                double intr = (due * ir) / 100;
+                due = due - (mi - intr);
+                payment += mi;
+                Console.WriteLine(String.Format("Month: {0} \t Balance: {1,-20} \t Total Payments: {2}",i,due,payment));
+
+                if (due <= 0) 
+                    dur = i;
+            }
+            Console.WriteLine("***********************************************************************");
+            return dur;
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your credit card amount due");
+            double credit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the monthly payment you wish to make");
+            double emi = double.Parse(Console.ReadLine());
+
+            CreditCalc c = new CreditCalc();
+            int mon=c.Calc(credit, emi);
+            Console.WriteLine("You will take {0} months to pay back {1}",mon,credit);
+            Console.ReadKey();
+
+        }
+    }
+}
